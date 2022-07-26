027894c058426f0cb0815b6e57e2ade400952926
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-07-26 18:59:27 -0300

    feat: arquivo de Models e Migrations da user, team e matches

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,49 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+class Matches extends Model {
+  id!: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+Matches.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    homeTeam: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    homeTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeam: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    timestamps: false,
+  },
+);
+
+export default Matches;
